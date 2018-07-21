17e7abeca15a5e7571e4b91b9b3109122776d70d
ustctf <feitian@outlook.com>
2018-07-21 04:57:13 -0700

    add trans script for deen

---
+++ run/translate_iwslt14_deen.sh
@@ -0,0 +1,1 @@
+python generate.py /home/fetia/transformer_pytorch/Transformer-PyTorch-master/data-bin/iwslt14.tokenized.de-en --path /home/fetia/ckpts/transformer/IWSLT14_DEEN/$1/checkpoint_best.pt --batch-size 128 --beam 5 --lenpen 1.0 --quiet --remove-bpe

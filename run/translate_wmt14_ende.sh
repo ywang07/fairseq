a88f423cad9e6f51954ae7908fdfe7ae567d75fd
ustctf <feitian@outlook.com>
2018-07-25 22:26:04 -0700

    Update local translate_wmt14_ende.sh

--- run/translate_wmt14_ende.sh
+++ run/translate_wmt14_ende.sh
@@ -1,16 +1,1 @@
-#NAME=$9
-model=transformer
-PROBLEM=WMT14_ENDE
-SETTING=transformer_vaswani_wmt_en_de_big
-
-REMOTE_DATA_PATH=/hdfs/sdrgvc/fetia/transformer_pytorch_data
-CODE_PATH=/var/storage/shared/sdrgvc/fetia/fairseq
-REMOTE_MODEL_PATH=/hdfs/sdrgvc/fetia/transformer_pytorch_model
-
-nvidia-smi
-
-python -c "import torch; print(torch.__version__)"
-
-
-
-python ${CODE_PATH}/generate.py ${REMOTE_DATA_PATH}/wmt14_en_de_joined_dict --path ${REMOTE_MODEL_PATH}/${model}/${PROBLEM}/${SETTING}/checkpoint_best.pt --batch-size 128 --beam 4 --lenpen 0.6 --quiet --remove-bpe --no-progress-bar
+python generate.py /home/fetia/fairseq/data-bin/wmt14_en_de_joined_dict --path /home/fetia/ckpts/transformer/WMT14_ENDE/$1/checkpoint_$2.pt --batch-size 128 --beam 5 --lenpen $3 --quiet --remove-bpe
